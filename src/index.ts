5d4e824843b30548bc4984a7174dffa51c6f493f
Matúš Tomlein <matus.tomlein@gmail.com>
2022-07-20 14:26:08 +0200

    Implement basic API communicating with the Android and iOS mobile trackers (close #1)

---
+++ src/index.ts
@@ -0,0 +1,155 @@
+import { SelfDescribingEvent, StructuredEvent, PageViewEvent, CommonEventProperties, SnowplowWebInterface, WebkitMessageHandler, ScreenView, SelfDescribingJson } from "./api";
+
+function withAndroidInterface(callback: (_: SnowplowWebInterface) => void) {
+  if (window.SnowplowWebInterface) {
+    callback(window.SnowplowWebInterface);
+  }
+}
+
+function withIOSInterface(callback: (_: WebkitMessageHandler) => void) {
+  if (window.webkit
+      && window.webkit.messageHandlers
+      && window.webkit.messageHandlers.snowplow) {
+    callback(window.webkit.messageHandlers.snowplow);
+  }
+}
+
+function serializeContext(context?: Array<SelfDescribingJson> | null) {
+  if (context) {
+    return JSON.stringify(context);
+  } else {
+    return undefined;
+  }
+}
+
+/**
+ * Track a self-describing event happening on this page.
+ * A custom event type, allowing for an event to be tracked using your own custom schema
+ * and a data object which conforms to the supplied schema
+ *
+ * @param event - The event information
+ * @param trackers - The tracker identifiers which the event will be sent to
+ */
+export function trackSelfDescribingEvent(event: SelfDescribingEvent & CommonEventProperties, trackers?: Array<string>) {
+  withAndroidInterface(webInterface => {
+    webInterface.trackSelfDescribingEvent(
+      event.event.schema,
+      JSON.stringify(event.event.data),
+      serializeContext(event.context),
+      trackers);
+  });
+
+  withIOSInterface(messageHandler => {
+    messageHandler.postMessage({
+      command: 'trackSelfDescribingEvent',
+      event: event.event,
+      context: event.context,
+      trackers: trackers
+    });
+  });
+};
+
+/**
+ * Track a structured event
+ * A classic style of event tracking, allows for easier movement between analytics
+ * systems. A loosely typed event, creating a Self Describing event is preferred, but
+ * useful for interoperability.
+ *
+ * @param event - The Structured Event properties
+ * @param trackers - The tracker identifiers which the event will be sent to
+ */
+export function trackStructEvent(event: StructuredEvent & CommonEventProperties, trackers?: Array<string>) {
+  withAndroidInterface(webInterface => {
+    webInterface.trackStructEvent(
+      event.category,
+      event.action,
+      event.label,
+      event.property,
+      event.value,
+      serializeContext(event.context),
+      trackers);
+  });
+
+  withIOSInterface(messageHandler => {
+    messageHandler.postMessage({
+      command: 'trackStructEvent',
+      event: {
+        category: event.category,
+        action: event.action,
+        label: event.label,
+        property: event.property,
+        value: event.value
+      },
+      context: event.context,
+      trackers: trackers
+    });
+  });
+}
+
+/**
+ * Track a visit to a web page
+ *
+ * @param event - The Page View Event properties
+ * @param trackers - The tracker identifiers which the event will be sent to
+ */
+export function trackPageView(event?: PageViewEvent & CommonEventProperties, trackers?: Array<string>) {
+  let url = window.location.href;
+  let title = event?.title ?? document.title;
+  let referrer = document.referrer;
+
+  withAndroidInterface(webInterface => {
+    webInterface.trackPageView(url, title, referrer, serializeContext(event?.context), trackers);
+  });
+
+  withIOSInterface(messageHandler => {
+    messageHandler.postMessage({
+      command: 'trackPageView',
+      event: {
+        url: url,
+        title: title,
+        referrer: referrer
+      },
+      context: event?.context,
+      trackers: trackers
+    });
+  });
+}
+
+/**
+ * Track a screen view event, which represents user viewing a screen within a mobile application.
+ *
+ * @param event - The event information
+ * @param trackers - The tracker identifiers which the event will be sent to
+ */
+export function trackScreenView(event: ScreenView & CommonEventProperties, trackers?: Array<string>) {
+  withAndroidInterface(webInterface => {
+    webInterface.trackScreenView(
+      event.name,
+      event.id,
+      event.type,
+      event.previousName,
+      event.previousId,
+      event.previousType,
+      event.transitionType,
+      serializeContext(event.context),
+      trackers
+    )
+  });
+
+  withIOSInterface(messageHandler => {
+    messageHandler.postMessage({
+      command: 'trackScreenView',
+      event: {
+        name: event.name,
+        id: event.id,
+        type: event.type,
+        previousName: event.previousName,
+        previousId: event.previousId,
+        previousType: event.previousType,
+        transitionType: event.transitionType,
+      },
+      context: event.context,
+      trackers: trackers
+    });
+  });
+}
---
+++ test/android.test.ts
@@ -0,0 +1,75 @@
+import { trackPageView, trackScreenView, trackSelfDescribingEvent, trackStructEvent } from '../src';
+
+describe('Android interface', () => {
+  let windowSpy: any;
+  let trackStructEventStub = jest.fn();
+  let trackScreenViewStub = jest.fn();
+  let trackPageViewStub = jest.fn();
+  let trackSelfDescribingStub = jest.fn();
+
+  beforeEach(() => {
+    windowSpy = jest.spyOn(window, 'window', 'get');
+    windowSpy.mockImplementation(() => ({
+      location: { href: 'http://test.com' },
+      SnowplowWebInterface: {
+        trackStructEvent: trackStructEventStub,
+        trackScreenView: trackScreenViewStub,
+        trackSelfDescribingEvent: trackSelfDescribingStub,
+        trackPageView: trackPageViewStub
+      }
+    }));
+  });
+
+  afterEach(() => {
+    windowSpy.mockRestore();
+  });
+
+  it('track a structured view', () => {
+    trackStructEvent({ category: 'cat', action: 'act' }, ['ns1', 'ns2']);
+    expect(trackStructEventStub).toHaveBeenCalledWith('cat', 'act', undefined, undefined, undefined, undefined, ['ns1', 'ns2'])
+  });
+
+  it('tracks a screen view', () => {
+    trackScreenView({ name: 'sv', id: 'xxx' })
+    expect(trackScreenViewStub).toHaveBeenCalledWith('sv', 'xxx', undefined, undefined, undefined, undefined, undefined, undefined, undefined);
+  });
+
+  it('tracks a self-describing event', () => {
+    trackSelfDescribingEvent({
+      event: {
+        schema: 'schema',
+        data: {
+          abc: 1
+        }
+      }
+    })
+
+    expect(trackSelfDescribingStub).toHaveBeenCalledWith('schema', '{"abc":1}', undefined, undefined);
+  });
+
+  it('tracks a page view', () => {
+    Object.defineProperty(global.document, 'title', { value: 'Title' });
+    Object.defineProperty(global.document, 'referrer', { value: 'http://referrer.com' });
+
+    trackPageView();
+    expect(trackPageViewStub).toHaveBeenCalledWith('http://test.com', 'Title', 'http://referrer.com', undefined, undefined);
+  });
+
+  it('adds context entities', () => {
+    trackStructEvent({
+      category: 'cat',
+      action: 'act',
+      context: [
+        {
+          schema: 'schema',
+          data: {
+            abc: 1
+          }
+        }
+      ]
+    }, undefined);
+
+    expect(trackStructEventStub).toHaveBeenCalledWith('cat', 'act', undefined, undefined, undefined, '[{"schema":"schema","data":{"abc":1}}]', undefined)
+  });
+
+});
