1decff56487590c2eeac3ac27f9b16a15abf0e9b
Matus Tomlein <matus.tomlein@gmail.com>
2022-10-03 14:11:02 +0200

    Add example app (close #6)
    PR #8

---
+++ example/src/App.js
@@ -0,0 +1,57 @@
+import logo from './logo.svg';
+import './App.css';
+import { trackSelfDescribingEvent, trackPageView, trackScreenView, trackStructEvent } from '@snowplow/webview-tracker';
+import { v4 as uuidv4 } from 'uuid';
+
+function trackEvents() {
+  trackSelfDescribingEvent({
+    event: {
+      schema: 'iglu:com.snowplowanalytics.snowplow/link_click/jsonschema/1-0-1',
+      data: {
+        targetUrl: 'http://a-target-url.com'
+      }
+    },
+    context: [
+      {
+        schema: 'iglu:com.snowplowanalytics.snowplow/link_click/jsonschema/1-0-1',
+        data: {
+          targetUrl: 'http://in-context.com'
+        }
+      }
+    ],
+  }, ['sp1']);
+
+  trackPageView();
+
+  trackStructEvent({
+    category: 'cat', action: 'act', property: 'prop', label: 'lbl', value: 10
+  });
+
+  trackScreenView({
+    name: 'SV1',
+    id: uuidv4(),
+    type: 'tp',
+    previousName: 'prevName',
+    previousId: uuidv4(),
+    previousType: 'prevType',
+    transitionType: 'transType',
+  });
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello from a Webview!
+        </p>
+        <button onClick={trackEvents}>
+          Track events
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
