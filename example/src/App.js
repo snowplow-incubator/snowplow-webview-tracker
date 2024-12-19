7659ab73ca466b63c417101900dbf336cf61bbfd
Miranda Wilson <miranda@snowplowanalytics.com>
2024-12-19 14:32:29 +0000

    Update example app

--- example/src/App.js
+++ example/src/App.js
@@ -1,30 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
-import { trackSelfDescribingEvent, trackPageView, trackScreenView, trackStructEvent } from '@snowplow/webview-tracker';
+import {
+  trackSelfDescribingEvent,
+  trackPageView,
+  trackScreenView,
+  trackStructEvent,
+  trackWebViewEvent,
+} from '@snowplow/webview-tracker';
 import { v4 as uuidv4 } from 'uuid';
 
 function trackEvents() {
-  trackSelfDescribingEvent({
-    event: {
+  trackWebViewEvent(
+    {
+      eventName: 'ue',
+      trackerVersion: 'webview',
+      useragent: 'useragent',
+    },
+    {
       schema: 'iglu:com.snowplowanalytics.snowplow/link_click/jsonschema/1-0-1',
       data: {
-        targetUrl: 'http://a-target-url.com'
-      }
+        targetUrl: 'http://a-target-url.com',
+      },
     },
-    context: [
+    [
       {
-        schema: 'iglu:com.snowplowanalytics.snowplow/link_click/jsonschema/1-0-1',
+        schema:
+          'iglu:com.snowplowanalytics.snowplow/link_click/jsonschema/1-0-1',
         data: {
-          targetUrl: 'http://in-context.com'
-        }
-      }
+          targetUrl: 'http://in-context.com',
+        },
+      },
     ],
-  }, ['sp1']);
+    ['sp1']
+  );
+
+  trackSelfDescribingEvent(
+    {
+      event: {
+        schema:
+          'iglu:com.snowplowanalytics.snowplow/link_click/jsonschema/1-0-1',
+        data: {
+          targetUrl: 'http://a-target-url.com',
+        },
+      },
+      context: [
+        {
+          schema:
+            'iglu:com.snowplowanalytics.snowplow/link_click/jsonschema/1-0-1',
+          data: {
+            targetUrl: 'http://in-context.com',
+          },
+        },
+      ],
+    },
+    ['sp1']
+  );
 
   trackPageView();
 
   trackStructEvent({
-    category: 'cat', action: 'act', property: 'prop', label: 'lbl', value: 10
+    category: 'cat',
+    action: 'act',
+    property: 'prop',
+    label: 'lbl',
+    value: 10,
   });
 
   trackScreenView({
@@ -43,12 +82,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello from a Webview!
-        </p>
-        <button onClick={trackEvents}>
-          Track events
-        </button>
+        <p>Hello from a Webview!</p>
+        <button onClick={trackEvents}>Track events</button>
       </header>
     </div>
   );
